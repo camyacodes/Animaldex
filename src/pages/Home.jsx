3a295ff57571c2dd183261b37477ca116669b974
Camya Robinson <camyacodes@gmail.com>
2024-07-23 19:57:38 -0400

    bg color and title

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Title from '../assets/animaldex_title.png'
+import Image from 'react-bootstrap/Image'
+
+const Home = () => {
+  return (
+    <div>
+      <Image src={Title} thumbnail />
+    </div>
+  )
+}
+
+export default Home
