8d4cab9100bc5ac3a9a66d24e444d79ba7b8b84d
Margaret A. <margaret.acheampong.dev@gmail.com>
2024-07-23 20:42:17 -0400

    homepage buttons

---
+++ src/components/Buttons/OpenDex/OpenDex.jsx
@@ -0,0 +1,8 @@
+import Button from 'react-bootstrap/Button';
+import './OpenDex.css'
+
+function OpenDex(){
+  return <Button className="OpenDex"> Open Dex </Button>;
+}
+
+export default OpenDex
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import Title from '../assets/animaldex_title.png'
 import Image from 'react-bootstrap/Image'
+import OpenDex from '../components/Buttons/OpenDex/OpenDex.jsx'
+import StartQuest from '../components/Buttons/StartQuest/StartQuest.jsx'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
       <Image src={Title} thumbnail />
+      <div className='homepage-buttons'>
+        <OpenDex/>
+        <StartQuest/>
+      </div>
     </div>
   )
 }
