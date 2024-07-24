508aa207c58b63e9f722cf8933902eeb6aedc13c
Camya Robinson <camyacodes@gmail.com>
2024-07-23 21:24:56 -0400

    Formatted home page buttons and title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,24 @@
 import Title from '../assets/animaldex_title.png'
-import Image from 'react-bootstrap/Image'
+// import Image from 'react-bootstrap/Image'
 import OpenDex from '../components/Buttons/OpenDex/OpenDex.jsx'
 import StartQuest from '../components/Buttons/StartQuest/StartQuest.jsx'
 import './Home.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 const Home = () => {
   return (
-    <div>
-      <Image src={Title} thumbnail />
-      <div className='homepage-buttons'>
-        <OpenDex/>
-        <StartQuest/>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <img src={Title} height='auto' width='350' />
+      </Row>
+      <Row>
+        <OpenDex />
+      </Row>
+      <Row>
+        <StartQuest />
+      </Row>
+    </Container>
   )
 }
 
