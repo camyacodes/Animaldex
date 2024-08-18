0848a2806e5ab7cf85d3c06473a08e7ca0ac7316
Camya Robinson <camyacodes@gmail.com>
2024-08-18 12:21:21 -0400

    added flexbox to images

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,18 +1,20 @@
 // import { Container, Row, Col } from 'react-bootstrap'
 // import Title from '../assets/animaldex_title.png'
-import DexItem from "../components/DexItem"
-import { getAnimalImage, getAnimalFacts } from "../services/animals_api"
-import animals_data from "../../animals.json"
+import DexItem from '../components/DexItem'
+// import { getAnimalImage, getAnimalFacts } from '../services/animals_api'
+import animals_data from '../../animals.json'
 
-const robin = { file_name: "American_Robin.png", name: "American Robin" }
+// const robin = { file_name: 'American_Robin.png', name: 'American Robin' }
 // const robin = { file_name: 'American_Robin.png', name: 'American Robin' }
 // console.log(animals_data)
 
 const Dex = () => {
   return (
-    <>
-      <DexItem name={robin.name} file_name={robin.file_name} />
-    </>
+    <div className='dex_container'>
+      {animals_data.map((animal, index) => (
+        <DexItem key={index} name={animal.name} file_name={animal.file_name} />
+      ))}
+    </div>
   )
 }
 
