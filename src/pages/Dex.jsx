33c86510777e98237f15cce8b14ef1165a09c4e9
Camya Robinson <camyacodes@gmail.com>
2024-08-25 12:48:26 -0400

    clean up code

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,22 +1,14 @@
 import { useEffect, useState } from 'react'
-// import Fuse from 'fuse.js'
-// import { Container, Row, Col } from 'react-bootstrap'
 // import Title from '../assets/animaldex_title.png'
 import DexItem from '../components/DexItem'
-// import { Search } from '../components/Search'
 import { getAnimalImage } from '../services/animals_api'
 import animals_data from '../../animals.json'
-
-// const robin = { file_name: 'American_Robin.png', name: 'American Robin' }
-// const robin = { file_name: 'American_Robin.png', name: 'American Robin' }
-// console.log(animals_data)
 
 const Dex = () => {
   const [searchValue, setSearchValue] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   const [animals, setAnimals] = useState()
-  // let animalsToShow
 
   useEffect(() => {
     const animalsWImages = animals_data.map((a) => {
@@ -25,14 +17,11 @@ const Dex = () => {
     })
     setAnimals(animalsWImages)
   }, [])
-  // console.log(animals)
-  // const fuse = new Fuse(animals, fuseOptions)
+
   const handleSearch = (event) => {
     event.preventDefault() // use to prevent page refresh
     setShowAll(false)
     setSearchValue(event.target.value)
-    // console.log(animals.filter((a) => a.name.includes(searchValue)))
-    // setAnimals(newAnimals)
   }
 
   const animalsToShow = showAll
@@ -41,7 +30,6 @@ const Dex = () => {
         a.name.toLowerCase().includes(searchValue.toLowerCase())
       )
 
-  // console.log(animalsToShow)
   return (
     <div id='dex_page'>
       <div id='search_bar'>
@@ -54,7 +42,6 @@ const Dex = () => {
           placeholder='Search animals..'
           onChange={handleSearch}
         />
-        {/* <Search /> */}
       </div>
       <div className='dex_container'>
         {animalsToShow ? (
