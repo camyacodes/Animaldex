230a82306d6cc23ac06ea5ba97bebc91151a204a
Camya Robinson <camyacodes@gmail.com>
2024-08-25 13:38:16 -0400

    animal facts api call on page

--- src/pages/Animal.jsx
+++ src/pages/Animal.jsx
@@ -1,27 +1,32 @@
+import { useState, useEffect } from 'react'
 import { getAnimalImage, getAnimalFacts } from '../services/animals_api'
 
-const Animal = async() => {
-    const test_animal =  { "file_name": "American_Robin.png", "name": "American Robin" }
-    const image = getAnimalImage(test_animal.file_name)
-
-    // useEffect(() => {
-    //     const animalsWImages = animals_data.map((a) => {
-    //       const image = getAnimalImage(a.file_name)
-    //       return { ...a, image: image }
-    //     })
-    //     setAnimals(animalsWImages)
-    //   }, [])
-
-    const result = await getAnimalFacts(test_animal)
-
-    console.log(result)
-    // console.log(getAnimalFacts(test_animal))
-    // console.log(getAnimalFacts(test_animal)[0])
-
-    return (
-        <img src={image} alt={test_animal.name} className='animal_image'></img>
-        
-    )
+const Animal = () => {
+  const [animalFacts, setAnimalFacts] = useState([{}])
+  const test_animal = {
+    file_name: 'American_Robin.png',
+    name: 'American Robin',
   }
-  
-  export default Animal
+  const image = getAnimalImage(test_animal.file_name)
+
+  useEffect(() => {
+    getAnimalFacts({
+      file_name: 'American_Robin.png',
+      name: 'American Robin',
+    }).then((facts) => setAnimalFacts(facts))
+  }, [])
+
+  console.log(animalFacts[0])
+  // scientific name animalFacts[0].taxonomy.scientific_name
+  // common name .name
+  // most distinctive feature .charateristics.most_distinctive_feature
+  // biggest threat .characteristics.biggest_threat
+
+  return (
+    <div>
+      <img src={image} alt={test_animal.name} className='animal_image'></img>
+    </div>
+  )
+}
+
+export default Animal
