1b819292cd05b8d6a62b091bcf0be8e5b6047da4
Camya Robinson <camyacodes@gmail.com>
2024-09-01 21:03:32 -0400

    search bar, timer, animal details and image, start quest,

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Dex from './pages/Dex'
 import Animal from './pages/Animal'
+import StartQuest from './pages/StartQuest'
+import Quest from './pages/Quest'
+import { useState } from 'react'
 
 function App() {
+  const [isTimerActive, setIsTimerActive] = useState(false)
+  const [time, setTime] = useState(0)
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/dex' element={<Dex />} />
         <Route path='dex/:animal' element={<Animal />} />
+        <Route
+          path='/startquest'
+          element={
+            <StartQuest
+              setIsTimerActive={setIsTimerActive}
+              isTimerActive={isTimerActive}
+            />
+          }
+        />
+        <Route
+          path='/quest'
+          element={
+            <Quest
+              setIsTimerActive={setIsTimerActive}
+              isTimerActive={isTimerActive}
+              setTime={setTime}
+              time={time}
+            />
+          }
+        />
       </Routes>
     </Router>
   )
---
+++ src/pages/Quest.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import Select from 'react-select'
+import Timer from '../components/Timer'
+
+import {
+  listBucketObjects,
+  getAnimalImage,
+  getAnimalFacts,
+} from '../services/animals_api'
+
+const Quest = ({ isTimerActive, setIsTimerActive, time, setTime }) => {
+  const [options, setOptions] = useState([
+    { label: 'loading...', value: 'loading...' },
+  ])
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [seenList, setSeenList] = useState([])
+  const [animalImage, setAnimalImage] = useState('')
+  const [animalFacts, setAnimalFacts] = useState('')
+
+  useEffect(() => {
+    const getOptions = async () => {
+      try {
+        const animalList = await listBucketObjects()
+
+        const options = animalList.map((a) => {
+          const name = a.split('.')[0].replaceAll('_', ' ')
+          return { label: name, value: name, image: a }
+        })
+        setOptions(options)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getOptions()
+  }, [])
+
+  useEffect(() => {
+    const selectedDetails = async () => {
+      try {
+        if (selectedOption) {
+          const animalImageURL = await getAnimalImage(selectedOption.image)
+          //   const animalFactPoints = await getAnimalFacts(selectedOption.label)
+
+          setAnimalImage(animalImageURL)
+          //   setAnimalFacts(animalFactPoints)
+        } else return
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    selectedDetails()
+  }, [selectedOption])
+
+  const MarkAsSeen = () => {
+    const newList = seenList.concat(selectedOption.label)
+    setSeenList(newList)
+    console.log(newList)
+  }
+
+  return (
+    <div>
+      <Timer isTimerActive={isTimerActive} time={time} setTime={setTime} />
+      <button
+        onClick={() => {
+          setIsTimerActive(false)
+        }}
+      >
+        End Quest
+      </button>
+      {selectedOption ? (
+        <div>
+          <h2>{animalFacts?.name || selectedOption.label}</h2>
+          <img src={animalImage} style={{ height: '300px', width: 'auto' }} />
+          <h3>
+            {/* Scientific Name: {animalFacts.taxonomy?.scientific_name || 'N/A'} */}
+          </h3>
+          <button onClick={MarkAsSeen}>Mark As Seen</button>
+        </div>
+      ) : (
+        <h2>Search for an animal</h2>
+      )}
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+      />
+    </div>
+  )
+}
+
+export default Quest
