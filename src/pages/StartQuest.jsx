59cb9c894dbb5b472868dc62198b54d7c71162cd
Camya Robinson <camyacodes@gmail.com>
2024-09-06 22:31:39 -0400

    styled homepage, and added styling to other pages

--- src/pages/StartQuest.jsx
+++ src/pages/StartQuest.jsx
@@ -3,11 +3,8 @@ import { Link } from 'react-router-dom'
 const StartQuest = ({ setIsTimerActive }) => {
   return (
     <div className='centered-content'>
-      <h1>AnimalDex Quest</h1>
-      <h2>
-        Get ready to embark on your animal-spotting adventure. <br />
-        Here’s how to get started:
-      </h2>
+      <h1 style={{ marginBottom: '0px' }}>AnimalDex Quest</h1>
+      <h2>Get ready to embark on your animal-spotting adventure!</h2>
       <ol>
         <li>Tap Start to begin and track time.</li>
         <li>Use the search bar and mark sightings.</li>
@@ -15,7 +12,7 @@ const StartQuest = ({ setIsTimerActive }) => {
         <li>Tap &ldquo;Stop Quest&rdquo; to save progress.</li>
       </ol>
 
-      <h2>Rules</h2>
+      <h2 style={{ fontSize: '2em', margin: '10px 0px' }}>Rules</h2>
       <ul>
         <li>Stay aware and avoid dangerous areas.</li>
         <li>Observe wildlife from afar, don’t disturb or feed.</li>
